104a549f3fd12e5120683f87440a575128a491ad
sarra <sarra.mejri@esprit.tn>
2023-02-28 13:52:46 +0100

    login fonctionnel avec session

--- Sportsverse_by_the_rescuers/src/Controller/HomeController.php
+++ Sportsverse_by_the_rescuers/src/Controller/HomeController.php
@@ -8,12 +8,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/client/home/{id}', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            
+
+        ]);
+    }
+    #[Route('/client/banned', name: 'app_clientBanned')]
+    public function client_banned(): Response
+    {
+        return $this->render('home/banned.html.twig');
+    }
+    #[Route('/coach/banned', name: 'app_coachBanned')]
+    public function coach_banned(): Response
+    {
+        return $this->render('home/banned.html.twig');
+    }
+
+
+    #[Route('/coach/homeCoach/{id}', name: 'app_home_coach')]
+    public function indexCoach(): Response
+    {
+        return $this->render('home/indexCoach.html.twig', [
+            'controller_name' => 'HomeController',
+
         ]);
     }
 }
--- Sportsverse_by_the_rescuers/src/Controller/UserController.php
+++ Sportsverse_by_the_rescuers/src/Controller/UserController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\AppUser;
 use App\Entity\User;
 use App\Form\User\UserType;
 use App\Form\User\LoginType;
 use App\Form\User\UpdateType;
+use App\Repository\AppUserRepository;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,6 +16,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+#[Route('/admin')]
 class UserController extends AbstractController
 {
     #[Route('/user', name: 'app_user')]
@@ -21,141 +24,91 @@ class UserController extends AbstractController
     {
         return $this->render('user/login.html.twig', [
             'controller_name' => 'UserController',
-            
+
         ]);
     }
 
-    #[Route('/signup', name: 'app_add_user', methods: ['GET', 'POST'])]
-    public function new(Request $request, UserRepository $userRepository): Response
+    #[Route('/disabled/{id}', name: 'app_user_banned')]
+    public function banned_user($id, AppUserRepository $userRepository): Response
     {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+        $user = $userRepository->findById($id);
+        $user->isBanned(true);
+        $userRepository->save($user);
+        $users = $userRepository->findAll();
+        //dd($users);
+        return $this->render('user/show.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+
+    #[Route('/dashboard/users', name: 'app_users_admin')]
+    public function Show(AppUserRepository $userRepository): Response
+    {
+        $users = $userRepository->findAll();
+        //dd($users);
+        return $this->render('user/show.html.twig', array(
+            'users' => $users,
+        ));
+    }
+
+    #[Route('/users/update/{id}', name: 'app_users_update', methods: ['GET', 'POST'])]
+    public function edit(Request $request, AppUser $user, AppUserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UpdateType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->save($user, true);
 
-            return $this->redirectToRoute('app_login', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_users_admin', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('user/create.html.twig', [
+        return $this->renderForm('user/update.html.twig', [
             'user' => $user,
             'form' => $form,
         ]);
     }
-#[Route('/login', name: 'app_login')]
-public function Login(Request $request,UserRepository $userRepository,ValidatorInterface $validator): Response
-    {
-        $user=new User();
-        $Form=$this->createForm(LoginType::class,$user);
-        $Form->handleRequest($request);
 
-        $errors = $validator->validate($user);
+    // #[Route('/users/delete', name: 'app_users_delete')]
+    // public function delete(Request $request,UserRepository $userRepository,ManagerRegistry $doctrine): Response
+    // {
+    //     $id = $request->query->get('id', 'null');
+    //     if($id!='null'){
+    //         $em = $doctrine->getManager();
+    //         $user = $userRepository->find($id);
+    //         $em->remove($user);
+    //         $em->flush();
+    //         return $this->redirectToRoute('app_users_admin');
+    //     }
+    //     return $this->redirectToRoute('app_users_admin');
+    // }
+    //   #[Route('users/delete/{id}', name: 'app_users_delete')]
+    //     public function delete(Request $request, $id) {
+    //         $user= $this->getDoctrine()->getRepository(User::class)->find($id);
 
-        if(count($errors) > 0){
-            return $this->render('user/login.html.twig', array(
-                'form'=>$Form->createView(),
-                'errors'=>$errors
-            ));
-        }
 
-        if ($Form->isSubmitted()&&$Form->isValid())/*verifier */
-        {
-            $userInDb = $userRepository->findOneBy(['email' => $user->getEmail()]);
-            if(!$userInDb){
-                return $this->render('user/login.html.twig', array(
-                    'form'=>$Form->createView(),
-                    'errors'=>array(),
-                    'message'=>"Utilisateur non trouvé dans notre base de données"
-                ));
-            }
-
-            if($userInDb->getPassword() != $user->getPassword()){
-                return $this->render('user/login.html.twig', array(
-                    'form'=>$Form->createView(),
-                    'errors'=>array(),
-                    'message'=>"Mot de passe incorrect"
-                ));
-            }
-            if($userInDb->getRole() == "admin"){
-                return $this->redirectToRoute('app_dashboard');
-            }
-            return $this->redirectToRoute('app_home');
-        }
-
-        return $this->render('user/login.html.twig', array(
-            'form'=>$Form->createView(),
-            'errors'=>array()
-        ));
-    }
-    #[Route('/dashboard/users', name: 'app_users_admin')]
-public function Show(UserRepository $userRepository): Response
-{
-    $users = $userRepository->findAll();
-    return $this->render('user/show.html.twig', array(
-        'users'=>$users,
-    ));
-}
-
-#[Route('/users/update/{id}', name: 'app_users_update', methods: ['GET', 'POST'])]
-public function edit(Request $request, User $user, UserRepository $userRepository): Response
-{
-    $form = $this->createForm(UpdateType::class, $user);
-    $form->handleRequest($request);
-
-    if ($form->isSubmitted() && $form->isValid()) {
-        $userRepository->save($user, true);
-
-        return $this->redirectToRoute('app_users_admin', [], Response::HTTP_SEE_OTHER);
-    }
-
-    return $this->renderForm('user/update.html.twig', [
-        'user' => $user,
-        'form' => $form,
-    ]);
-}
-
-// #[Route('/users/delete', name: 'app_users_delete')]
-// public function delete(Request $request,UserRepository $userRepository,ManagerRegistry $doctrine): Response
-// {
-//     $id = $request->query->get('id', 'null');
-//     if($id!='null'){
-//         $em = $doctrine->getManager();
-//         $user = $userRepository->find($id);
-//         $em->remove($user);
-//         $em->flush();
-//         return $this->redirectToRoute('app_users_admin');
-//     }
-//     return $this->redirectToRoute('app_users_admin');
-// }
-//   #[Route('users/delete/{id}', name: 'app_users_delete')]
-//     public function delete(Request $request, $id) {
-//         $user= $this->getDoctrine()->getRepository(User::class)->find($id);
-
-    
-//         $entityManager = $this->getDoctrine()->getManager();
-//         $entityManager->remove($user);
-//         $entityManager->flush();
-//         $response = new Response();
-//         $response->send();
-//         return $this->redirectToRoute('app_users_admin');
-//     }
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->remove($user);
+    //         $entityManager->flush();
+    //         $response = new Response();
+    //         $response->send();
+    //         return $this->redirectToRoute('app_users_admin');
+    //     }
     #[Route('/{id}', name: 'app_users_delete', methods: ['POST'])]
-    public function delete(Request $request, Student $student, StudentRepository $studentRepository): Response
+    public function delete(Request $request, User $student, UserRepository $studentRepository): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$student->getNSC(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $student->getId(), $request->request->get('_token'))) {
             $studentRepository->remove($student, true);
         }
 
         return $this->redirectToRoute('app_users_admin', [], Response::HTTP_SEE_OTHER);
     }
     #[Route('/{id}', name: 'app_user_afficher', methods: ['GET'])]
-       public function afficher(User $user): Response
+    public function afficher(AppUser $user): Response
     {
         return $this->render('user/afficher.html.twig', [
             'user' => $user,
         ]);
     }
-
-
 }
