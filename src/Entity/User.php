08ab48e17fc2671e0d7f2043eb8709c1aec580ff
AyoubHdhili <124737650+AyoubHdhili@users.noreply.github.com>
2023-03-06 14:22:18 +0100

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -312,6 +312,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         }
 
         return $this;
+    }
     public function __toString()
     {
         return (string) $this->nom;
